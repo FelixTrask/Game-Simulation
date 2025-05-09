602a9c5cba4e6453a35bb82f1efc224f0076efec
FelixTrask <felixtrask@gmail.com>
2025-05-09 13:20:01 -0400

    Update it back

--- Game-Simulator.py
+++ Game-Simulator.py
@@ -10,24 +10,24 @@ full_deck = [(rank, suit) for rank in ranks for suit in suits]
 def draw_card(card_number):
     cards = full_deck.copy()
     card_list = []
-    for _ in range(card_number):
+    for i in range(card_number):
         random_card = random.choice(cards)
         card_list.append(random_card)
         cards.remove(random_card)
     return card_list
 
 money = 0
-times_ran = 10000000
+times_ran = 1000000
 cards_drawn = 5
 
 count_3_of_a_kind = 0
 count_3_face_cards = 0
 count_3_same_suit = 0
 count_3_non_face = 0
-count_5_same_color = 0
+count_3_same_color = 0
 count_no_win = 0
 
-for _ in range(times_ran):
+for i in range(times_ran):
     money -= 10
     card_list = draw_card(cards_drawn)
 
@@ -62,24 +62,25 @@ for _ in range(times_ran):
         count_3_non_face += 1
         is_win = True
 
-    if red_count == 5 or black_count == 5:
+    if (red_count == 3 and black_count == 2) or (black_count == 3 and red_count == 2):
         money += 5
-        count_5_same_color += 1
+        count_3_same_color += 1
         is_win = True
 
     if not is_win:
         count_no_win += 1
 
+
 print("Average money loss/gain: " + str(money / times_ran) + "\n")
 print("3 of a kind hits:", count_3_of_a_kind)
-print("3 of a kind percentage:", str(100 * (count_3_of_a_kind / times_ran)), "\n")
+print("3 of a kind percentage:", str(100*(count_3_of_a_kind/times_ran)), "\n")
 print("3 face cards hits:", count_3_face_cards)
-print("3 face card percentage:", str(100 * (count_3_face_cards / times_ran)), "\n")
+print("3 face card percentage:", str(100*(count_3_face_cards/times_ran)), "\n")
 print("3 same suit hits:", count_3_same_suit)
-print("3 same suit hits percentage:", str(100 * (count_3_same_suit / times_ran)), "\n")
+print("3 same suit hits percentage:", str(100*(count_3_same_suit/times_ran)), "\n")
 print("3 non-face cards hits:", count_3_non_face)
-print("3 non-face cards percentage:", str(100 * (count_3_non_face / times_ran)), "\n")
-print("5 same color hits:", count_5_same_color)
-print("5 same color percentage:", str(100 * (count_5_same_color / times_ran)), "\n")
+print("3 non-face cards percentage:", str(100*(count_3_non_face/times_ran)), "\n")
+print("3 same color hits:", count_3_same_color)
+print("3 same color percentage:", str(100*(count_3_same_color/times_ran)), "\n")
 print("No win hands:", count_no_win)
-print("No win percentage:", str(100 * (count_no_win / times_ran)), "\n")
+print("No win percentage:", str(100*(count_no_win/times_ran)), "\n")
